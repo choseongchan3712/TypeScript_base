74ec17ac2e53d0eed85381d78bfc062f61534d54
choseongchan3712 <choseongchan3712@naver.com>
2025-08-05 03:13:12 +0900

    TypeScript start

---
+++ main.ts
@@ -0,0 +1,11 @@
+// ? node 설치: npm init
+
+// ? 타입스크립트 설치: npm install --seva-dev typescript
+
+// ? tsconfig.json 설치: npx tsc --init
+
+// ? npm run build 를 실행하기 위해: "build":"tsc"
+
+// ? npm start 를 실행하기 위해: "start":"node main.js"
+
+console.log('Hello TypeScript')
